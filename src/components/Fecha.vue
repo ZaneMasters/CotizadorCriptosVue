cb7bc78ff2a37f4fa40e2cfd0de5f9ebbb0d8d78
ZaneMasters <angel.pro98@gmail.com>
2024-06-14 14:34:16 -0500

    first commit

---
+++ src/components/Fecha.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card">
+      <p>Hoy: {{ currentDate }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted } from 'vue';
+  
+  export default {
+    setup() {
+      const currentDate = ref('');
+  
+      const updateCurrentDate = () => {
+        const today = new Date();
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        currentDate.value = today.toLocaleDateString('es-ES', options);
+      };
+  
+      onMounted(() => {
+        updateCurrentDate();
+      });
+  
+      return {
+        currentDate
+      };
+    }
+  };
+  </script>
+
+
+<style>
+.card {
+  position: fixed;
+  right: 20px;
+  top: 20px;
+  padding: 8px;
+  background-color: #f8f9fa;
+  border: 1px solid #dee2e6;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  font-family: Arial, sans-serif;
+  color: #343a40;
+}
+</style>
